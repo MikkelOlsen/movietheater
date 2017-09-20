1c15bd920d1e2e31b4b3ddbae588c7c60b076070
MA Web <staal.mikkel@gmail.com>
2017-09-20 23:22:27 +0200

    Rev 2

--- frontend/includes/list.php
+++ frontend/includes/list.php
@@ -3,20 +3,9 @@
         <img src="./image/plakater/alien_vs_predator_2_poster.jpg" width="65" class="col s4" alt="">
         <div class="col s8">
             <h4>Overskrift</h4>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-        </div>
-    </div>
-    <div class="row">
-        <img src="./image/plakater/alien_vs_predator_2_poster.jpg" width="65" class="col s4" alt="">
-        <div class="col s8">
-            <h4>Overskrift</h4>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star_half</i>
             <i class="material-icons">star_border</i>
             <i class="material-icons">star_border</i>
         </div>
@@ -25,20 +14,9 @@
         <img src="./image/plakater/alien_vs_predator_2_poster.jpg" width="65" class="col s4" alt="">
         <div class="col s8">
             <h4>Overskrift</h4>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-        </div>
-    </div>
-    <div class="row">
-        <img src="./image/plakater/alien_vs_predator_2_poster.jpg" width="65" class="col s4" alt="">
-        <div class="col s8">
-            <h4>Overskrift</h4>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
             <i class="material-icons">star_border</i>
             <i class="material-icons">star_border</i>
         </div>
@@ -47,9 +25,31 @@
         <img src="./image/plakater/alien_vs_predator_2_poster.jpg" width="65" class="col s4" alt="">
         <div class="col s8">
             <h4>Overskrift</h4>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
+        </div>
+    </div>
+    <div class="row">
+        <img src="./image/plakater/alien_vs_predator_2_poster.jpg" width="65" class="col s4" alt="">
+        <div class="col s8">
+            <h4>Overskrift</h4>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
             <i class="material-icons">star_border</i>
             <i class="material-icons">star_border</i>
-            <i class="material-icons">star_border</i>
+        </div>
+    </div>
+    <div class="row">
+        <img src="./image/plakater/alien_vs_predator_2_poster.jpg" width="65" class="col s4" alt="">
+        <div class="col s8">
+            <h4>Overskrift</h4>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star</i>
+            <i class="material-icons">star_half</i>
             <i class="material-icons">star_border</i>
             <i class="material-icons">star_border</i>
         </div>
