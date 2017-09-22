fb45e51d4a18acb89a2875344479c72ea1954ab4
MA Web <staal.mikkel@gmail.com>
2017-09-22 13:00:16 +0200

    Link on random movie

--- frontend/includes/home.php
+++ frontend/includes/home.php
@@ -3,7 +3,7 @@
 ?>
 <section class="row">
     <div class="col s5">
-        <img src="./image/plakater/<?= $random->img_path ?>" alt="">
+        <a href="?p=movie&id=<?= $random->movie_id ?>"><img src="./image/plakater/<?= $random->img_path ?>" alt=""></a>
     </div>
     <div class="col s7">
         <h3><?= $random->movie_name ?></h3>
